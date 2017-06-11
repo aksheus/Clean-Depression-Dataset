44cd98b0a576d263cd4a11d73a013ee6714a0623
akshay kulkarni <kumar.akshay.kulkarni@gmail.com>
2017-06-11 15:31:49 -0500

    added method to get text data

--- CleanDepressionDataset/Program.cs
+++ CleanDepressionDataset/Program.cs
@@ -8,10 +8,18 @@ namespace CleanDepressionDataset
 {
     class Program
     {
+        private static readonly string RequiredTag = "TEXT";
+        private static readonly string ParentTag = "WRITING";
         static void Main(string[] args)
         {
-            TagData tg = new TagData("C:\\Users\\abkma\\reddit-depression\\testing\\chunk_1\\test_subject25_1.xml");
-            tg.PrintContent();
+            
+            XmlReader Reader = new XmlReader("C:\\Users\\abkma\\reddit-depression\\testing\\chunk_1\\test_subject25_1.xml");
+            List<string> Output = Reader.GetTagData(RequiredTag,ParentTag);
+            foreach(string s in Output)
+            {
+                Console.WriteLine(s);
+            }
+
             Console.ReadKey();
         }
     }
---
+++ CleanDepressionDataset/XmlReader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace CleanDepressionDataset
+{
+    class XmlReader
+    {
+        private string XmlFile;
+        private XmlDocument Document;
+
+        public XmlReader(string xmlFile)
+        {
+            XmlFile = xmlFile;
+            Document = new XmlDocument();
+            Document.Load(xmlFile);
+        }
+
+
+        public List<string> GetTagData(string requiredTag,string parentTag)
+        {
+            List<string> TextData = new List<string>();
+
+            foreach (XmlNode Node in Document.DocumentElement.ChildNodes)
+            {
+                if(Node.Name == parentTag && Node.HasChildNodes)
+                {
+                    foreach(XmlNode Child in Node.ChildNodes)
+                    {
+                        if (Child.Name == requiredTag)
+                        {
+                            TextData.Add(Child.InnerText);
+                        }
+                    }
+                }
+               
+            }
+
+            return TextData;
+        }
+
+        public void PrintContent()
+        {
+            foreach(XmlNode Node in Document.DocumentElement.ChildNodes)
+            {
+                Console.WriteLine(Node.InnerText);
+            }
+        }
+    }
+}
