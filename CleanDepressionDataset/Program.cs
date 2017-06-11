3fb778d8958b3f3e7ec0e34eeecaaf9485685dcb
akshay kulkarni <kumar.akshay.kulkarni@gmail.com>
2017-06-11 15:05:49 -0500

    added tag data class

--- CleanDepressionDataset/Program.cs
+++ CleanDepressionDataset/Program.cs
@@ -10,6 +10,9 @@ namespace CleanDepressionDataset
     {
         static void Main(string[] args)
         {
+            TagData tg = new TagData("C:\\Users\\abkma\\reddit-depression\\testing\\chunk_1\\test_subject25_1.xml");
+            tg.PrintContent();
+            Console.ReadKey();
         }
     }
 }
