a313e9e75946f99a05867bae8ade671f66ca9baa
akshay kulkarni <kumar.akshay.kulkarni@gmail.com>
2017-06-13 15:28:54 -0500

    added file combiner class and tentative code for chunk combining

---
+++ CleanDepressionDataset/FileCombiner.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CleanDepressionDataset
+{
+    class FileCombiner
+    {
+        private List<string> Files;
+        private string WritePath;
+        private bool Alright;
+        private List<string> CombinedContent;
+
+        public bool IsItAlright { get => Alright; set => Alright = value; }
+
+        public FileCombiner (List<string> files,string writePath)
+        {
+            Files = files;
+            WritePath = writePath;
+            Alright = true;
+        }
+
+        public void WriteCombinedFiles(string newFile)
+        {
+            CombineFiles();
+            File.WriteAllLines(newFile, CombinedContent.ToArray());
+        }
+
+        public void CombineFiles()
+        {
+            try
+            {
+                foreach (string FileToRead in Files)
+                {
+                    if (File.Exists(FileToRead))
+                    {
+                        foreach (string line in File.ReadAllLines(FileToRead))
+                        {
+                            CombinedContent.Add(line);
+                        }
+
+                        CombinedContent.Add("\n");
+                        CombinedContent.Add("\n");
+                        CombinedContent.Add("\n");
+                        CombinedContent.Add("\n");
+                        CombinedContent.Add("\n");
+                        CombinedContent.Add("\n");
+                        CombinedContent.Add("\n");
+                        CombinedContent.Add("\n");
+                        CombinedContent.Add("\n");
+                        CombinedContent.Add("\n");
+
+                    }
+                }
+            }
+            catch (Exception Excep)
+            {
+                IsItAlright = false;
+            }
+        }
+
+    }
+}
--- CleanDepressionDataset/Program.cs
+++ CleanDepressionDataset/Program.cs
@@ -8,41 +8,81 @@ namespace CleanDepressionDataset
     {
         private static readonly string RequiredTag = "TEXT";
         private static readonly string ParentTag = "WRITING";
-        private static readonly string WriteDirectory = @"C:\Users\abkma\reddit-depression\training\cleaned_negative";
-        private static readonly string ReadDirectory = @"C:\Users\abkma\reddit-depression\training\negative_examples_anonymous_chunks";
-
+        private static readonly string WriteDirectory = @"C:\Users\abkma\reddit-depression\training\cleaned_positive";
+        private static readonly string ReadDirectory = @"C:\Users\abkma\reddit-depression\training\positive_examples_anonymous_chunks";
+        private static readonly int NumberOfFiles = 401;
+        private static readonly string[] Chunks = new string[]
+        {
+            "\\chunk_1",
+            "\\chunk_2",
+            "\\chunk_3",
+            "\\chunk_4",
+            "\\chunk_5",
+            "\\chunk_6",
+            "\\chunk_7",
+            "\\chunk_8",
+            "\\chunk_9",
+            "\\chunk_10"
+        };
         static void Main(string[] args)
         {
-            string[] Directories = Directory.GetDirectories(ReadDirectory);
 
-            foreach ( string Direc in Directories)
+            string FirstChunk = Directory.GetFiles(ReadDirectory + Chunks[0])[0];
+
+            for ( int Index = 1; Index < Chunks.Length; Index++)
             {
-                foreach(string XmlFile in Directory.GetFiles(Direc))
+                List<string> FilesToCombine = new List<string>();
+                string NextChunk;
+                foreach (string s in Directory.GetFiles(ReadDirectory + Chunks[Index]))
                 {
-                    XmlReader Reader = new XmlReader(XmlFile);
-                    if (Reader.IsItAlright)
+                    if (IsTheNextChunk(FirstChunk, s))
                     {
-                        List<string> Output = Reader.GetTagData(RequiredTag, ParentTag);
-                        Writer Write = new Writer();
-                        Write.WriteToTxt(PreprocessPath(XmlFile), Output);
+                        NextChunk = s;
                     }
                 }
-
             }
+                 
+
+            // For single chunks 
+            /*
+                 string[] Directories = Directory.GetDirectories(ReadDirectory);
+             * 
+             *   foreach ( string Direc in Directories)
+                 {
+                    foreach(string XmlFile in Directory.GetFiles(Direc))
+                    {
+                        XmlReader Reader = new XmlReader(XmlFile);
+                        if (Reader.IsItAlright)
+                        {
+                            List<string> Output = Reader.GetTagData(RequiredTag, ParentTag);
+                            Writer Write = new Writer();
+                            Write.WriteToTxt(PreprocessPath(XmlFile), Output);
+                        }
+                    }
+                 }
+
+            */
 
             Console.ReadKey();
         }
 
         public static string PreprocessPath(string readFilePath)
         {
-            string  [] Temp = readFilePath.Split('\\');
+            string[] Temp = readFilePath.Split('\\');
 
-            return WriteDirectory 
-                + "\\" 
-                + Temp[Temp.Length-2]
+            return WriteDirectory
                 + "\\"
-                + Temp[Temp.Length-1].Split('.').First()
-                + ".txt";
+                + Temp[Temp.Length - 2]
+                + "\\"
+                + Temp[Temp.Length - 1].Split('.').First()
+               + ".txt";
         }
+
+        public static bool IsTheNextChunk(string first, string second)
+        {
+            return first.Substring(0, first.Length - 2)
+                        .Equals(second.Substring(0, second.Length - 2));
+        }
+        
     }
 }
