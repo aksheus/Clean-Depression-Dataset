b2f6424902b0d37be43cea838aa79a443a27107c
akshay kulkarni <kumar.akshay.kulkarni@gmail.com>
2017-08-24 19:07:31 -0500

    reading day,month,year

--- python_scripts/user-activity.py
+++ python_scripts/user-activity.py
@@ -3,17 +3,16 @@ import csv
 import os
 import sys
 """
-USAGE : python user-activity.py path_to_csv_file"""
+USAGE : python user-activity.py path_to_csv_file1 path_to_csvfile2 path_to_csv_file3 bar_graph_title boxplot_title"""
 
-if __name__ == '__main__':
-
+def get_stuff(filename):
 	subjects = []
 	averages = []
 	standard_deviations = []
 	grand_mean,minimum,maximum ,grand_sd = None,None,None,None 
 
-	if os.path.exists(sys.argv[1]):
-		with open(sys.argv[1],'r') as csvfile:
+	if os.path.exists(filename):
+		with open(filename,'r') as csvfile:
 			reader = csv.reader(csvfile,delimiter=',')
 			for row in list(reader)[1:]:
 				if row[-1] !='':
@@ -30,12 +29,26 @@ if __name__ == '__main__':
 					elif row[0] == 'Grand SD of Means ' :
 						grand_sd = float(row[1])
 	else:
-		print 'Csv File Does not Exist'
+		print 'Csv File Does not Exist : {0}'.format(filename)
 		exit(1)
+ 
+	return subjects,averages,standard_deviations,grand_mean,minimum,maximum,grand_sd
 
-def plot_bargraph(values,categories):
+
+def plot_bargraph(title,values,categories):
 	pass
 
 def plot_boxplot(values,categories):
 	pass 
+
+if __name__ == '__main__':
+
+	subjects1,averages1,standard_deviations1,grand_mean1,minimum1,maximum1,grand_sd1 = get_stuff(sys.argv[1])
+	subjects2,averages2,standard_deviations2,grand_mean2,minimum2,maximum2,grand_sd2 = get_stuff(sys.argv[2])
+	subjects3,averages3,standard_deviations3,grand_mean3,minimum3,maximum3,grand_sd3 = get_stuff(sys.argv[3])
+
+
+
+
+
 
